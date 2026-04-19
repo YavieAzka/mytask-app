6aacfe3b114651866c94040e3a3d4990d064f73c
YavieAzka <yavieazkaputra@gmail.com>
2026-04-19 11:56:21 +0700

    fix gitignore

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../../src/generated/prisma";
 
 const prismaClientSingleton = () => {
   const connectionString = process.env.DATABASE_URL;
